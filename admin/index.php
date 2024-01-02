42a2c1965cf3ee2fd6a3c978c2a52193ae3d56e1
Michael <wariorstore17@gmail.com>
2024-01-02 07:58:47 +0700

    Commit First

---
+++ admin/index.php
@@ -0,0 +1,32 @@
+<html>
+    <head>
+        <title>Food Order Website - Home Page</title>
+
+        <link rel="stylesheet" href="../css/admin.css">
+    </head>
+    <body>
+        <!------ Menu Section Start------>
+        <div class="menu">
+            <div class="wrapper">
+                Menu Goes Here
+            </div>
+        </div>
+        <!------ Menu Section End------>
+
+        <!------ Main Contenct Section Start------>
+        <div class="main-content">
+            <div class="wrapper">
+                Main Content Goes Here
+            </div>
+        </div>
+        <!------ Main Contenct Section End------>
+
+        <!------ Footer Section Starts------>
+        <div class="footer">
+            <div class="wrapper">
+                Footer Goes Here
+            </div>
+        </div>
+        <!------ Footer Section End------>
+    </body>
+</html>
